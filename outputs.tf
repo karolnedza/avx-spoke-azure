a158d227479f94aad58ecd5bdfe0f71374ac8092
karolnedza <37583943+karolnedza@users.noreply.github.com>
2020-11-25 15:35:54 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -7,11 +7,15 @@ output "private_ip_address_windows" {
 value = azurerm_network_interface.win-iface.private_ip_address
 }
 
-
 output "public_ip_address_ubuntu" {
   value = azurerm_public_ip.ubuntu-public-ip.ip_address
 }
 
 output "private_ip_address_ubuntu" {
 value = azurerm_network_interface.iface.private_ip_address
+}
+
+output "vpc_subnets" {
+ value = aviatrix_vpc.aviatrix_vpc_vnet.subnets[*]
+  
 }
