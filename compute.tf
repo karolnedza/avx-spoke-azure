3e6a4a049ad8c5eb3329c6bdd8485c61bb2aa7dc
karolnedza <37583943+karolnedza@users.noreply.github.com>
2020-12-10 15:51:07 +0100

    Update compute.tf

--- compute.tf
+++ compute.tf
@@ -3,132 +3,132 @@
 ################################################################
 
 
-#### Resource Group
+# #### Resource Group
 
-resource "azurerm_resource_group" "aviatrix-rg" {
-  name     = "rg-${var.vm_name}"
-  location = var.cloud_region["${var.aviatrix_transit_gateway}"]
-}
+# resource "azurerm_resource_group" "aviatrix-rg" {
+#   name     = "rg-${var.vm_name}"
+#   location = var.cloud_region["${var.aviatrix_transit_gateway}"]
+# }
 
-#### Public IP 
+# #### Public IP 
 
-resource "azurerm_public_ip" "ubuntu-public-ip" {
-  name                = "u-ip-${var.vm_name}"
-  location            = azurerm_resource_group.aviatrix-rg.location
-  resource_group_name = azurerm_resource_group.aviatrix-rg.name
-  allocation_method   = "Static"
- }
+# resource "azurerm_public_ip" "ubuntu-public-ip" {
+#   name                = "u-ip-${var.vm_name}"
+#   location            = azurerm_resource_group.aviatrix-rg.location
+#   resource_group_name = azurerm_resource_group.aviatrix-rg.name
+#   allocation_method   = "Static"
+#  }
 
-#### VM interface 
-resource "azurerm_network_interface" "iface" {
-  name                = "nic-${var.vm_name}"
-  location            = azurerm_resource_group.aviatrix-rg.location
-  resource_group_name = azurerm_resource_group.aviatrix-rg.name
+# #### VM interface 
+# resource "azurerm_network_interface" "iface" {
+#   name                = "nic-${var.vm_name}"
+#   location            = azurerm_resource_group.aviatrix-rg.location
+#   resource_group_name = azurerm_resource_group.aviatrix-rg.name
 
-  ip_configuration {
-    name                          = "avtx_internal-${var.vm_name}"
-    subnet_id = aviatrix_vpc.aviatrix_vpc_vnet.subnets[1].subnet_id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id = azurerm_public_ip.ubuntu-public-ip.id
+#   ip_configuration {
+#     name                          = "avtx_internal-${var.vm_name}"
+#     subnet_id = aviatrix_vpc.aviatrix_vpc_vnet.subnets[1].subnet_id
+#     private_ip_address_allocation = "Dynamic"
+#     public_ip_address_id = azurerm_public_ip.ubuntu-public-ip.id
 
-  }
-}
+#   }
+# }
 
-#### VM 
+# #### VM 
 
-resource "azurerm_linux_virtual_machine" "azure-spoke-vm" {
-  name                = "u${var.vm_name}"
-  resource_group_name = azurerm_resource_group.aviatrix-rg.name
-  location            = azurerm_resource_group.aviatrix-rg.location
-  size                = "Standard_B1s"
-  admin_username      = "ubuntu"
-  network_interface_ids = [
-    azurerm_network_interface.iface.id,
-  ]
+# resource "azurerm_linux_virtual_machine" "azure-spoke-vm" {
+#   name                = "u${var.vm_name}"
+#   resource_group_name = azurerm_resource_group.aviatrix-rg.name
+#   location            = azurerm_resource_group.aviatrix-rg.location
+#   size                = "Standard_B1s"
+#   admin_username      = "ubuntu"
+#   network_interface_ids = [
+#     azurerm_network_interface.iface.id,
+#   ]
 
-  admin_password = "Aviatrix123#"
-  disable_password_authentication = "false"
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
+#   admin_password = "Aviatrix123#"
+#   disable_password_authentication = "false"
+#   os_disk {
+#     caching              = "ReadWrite"
+#     storage_account_type = "Standard_LRS"
+#   }
 
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  }
-}
+#   source_image_reference {
+#     publisher = "Canonical"
+#     offer     = "UbuntuServer"
+#     sku       = "18.04-LTS"
+#     version   = "latest"
+#   }
+# }
 
-####################################################################
-# Azure Ubuntu Instance
-################################################################
+# ####################################################################
+# # Azure Ubuntu Instance
+# ################################################################
 
 
-#### Public IP 
+# #### Public IP 
 
-resource "azurerm_public_ip" "windows-public-ip" {
-  name                = "w-ip-${var.vm_name}"
-  location            = azurerm_resource_group.aviatrix-rg.location
-  resource_group_name = azurerm_resource_group.aviatrix-rg.name
-  allocation_method   = "Static"
- }
+# resource "azurerm_public_ip" "windows-public-ip" {
+#   name                = "w-ip-${var.vm_name}"
+#   location            = azurerm_resource_group.aviatrix-rg.location
+#   resource_group_name = azurerm_resource_group.aviatrix-rg.name
+#   allocation_method   = "Static"
+#  }
 
-#### VM interface 
+# #### VM interface 
 
-resource "azurerm_network_interface" "win-iface" {
-  name                = "nic-windows-${var.vm_name}"
-  location            = azurerm_resource_group.aviatrix-rg.location
-  resource_group_name = azurerm_resource_group.aviatrix-rg.name
+# resource "azurerm_network_interface" "win-iface" {
+#   name                = "nic-windows-${var.vm_name}"
+#   location            = azurerm_resource_group.aviatrix-rg.location
+#   resource_group_name = azurerm_resource_group.aviatrix-rg.name
 
-  ip_configuration {
-    name                          = "windows-${var.vm_name}"
-    subnet_id = aviatrix_vpc.aviatrix_vpc_vnet.subnets[1].subnet_id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id = azurerm_public_ip.windows-public-ip.id
+#   ip_configuration {
+#     name                          = "windows-${var.vm_name}"
+#     subnet_id = aviatrix_vpc.aviatrix_vpc_vnet.subnets[1].subnet_id
+#     private_ip_address_allocation = "Dynamic"
+#     public_ip_address_id = azurerm_public_ip.windows-public-ip.id
 
-  }
-}
+#   }
+# }
 
-#### VM 
+# #### VM 
 
-resource "azurerm_windows_virtual_machine" "azure-spoke-vm" {
-  name                = "w${var.vm_name}"
-  resource_group_name = azurerm_resource_group.aviatrix-rg.name
-  location            = azurerm_resource_group.aviatrix-rg.location
-  size                = "Standard_D2_v2"
-  admin_username      = "aviatrix"
-  admin_password = "Aviatrix123#"
+# resource "azurerm_windows_virtual_machine" "azure-spoke-vm" {
+#   name                = "w${var.vm_name}"
+#   resource_group_name = azurerm_resource_group.aviatrix-rg.name
+#   location            = azurerm_resource_group.aviatrix-rg.location
+#   size                = "Standard_D2_v2"
+#   admin_username      = "aviatrix"
+#   admin_password = "Aviatrix123#"
   
-  network_interface_ids = [
-    azurerm_network_interface.win-iface.id,
-  ]
+#   network_interface_ids = [
+#     azurerm_network_interface.win-iface.id,
+#   ]
 
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
+#   os_disk {
+#     caching              = "ReadWrite"
+#     storage_account_type = "Standard_LRS"
+#   }
   
-  source_image_reference { 
-    publisher="MicrosoftWindowsDesktop"
-    offer="Windows-10"
-    sku="20h1-pro"
-    version="latest"
-  }
-}
+#   source_image_reference { 
+#     publisher="MicrosoftWindowsDesktop"
+#     offer="Windows-10"
+#     sku="20h1-pro"
+#     version="latest"
+#   }
+# }
 
-############ DNS 
+# ############ DNS 
 
-data "aws_route53_zone" "pub" {
-  name         = "mcna.cc"
-  private_zone = false
-}
+# data "aws_route53_zone" "pub" {
+#   name         = "mcna.cc"
+#   private_zone = false
+# }
 
-resource "aws_route53_record" "azure_vm_fqdn" {
-  zone_id    = data.aws_route53_zone.pub.zone_id
-  name       = "${var.vm_name}-win.pub.mcna.cc"
-  type       = "A"
-  ttl        = "300"
-  records    = [azurerm_public_ip.windows-public-ip.ip_address]
-}
+# resource "aws_route53_record" "azure_vm_fqdn" {
+#   zone_id    = data.aws_route53_zone.pub.zone_id
+#   name       = "${var.vm_name}-win.pub.mcna.cc"
+#   type       = "A"
+#   ttl        = "300"
+#   records    = [azurerm_public_ip.windows-public-ip.ip_address]
+# }
